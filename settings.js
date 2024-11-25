6f8f8e71c03556c03caeeb785c244d87ac40d9a9
Ben-kerm <hanos7275@gmail.com>
2024-11-25 14:17:01 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,13 +9,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "243891716833"
+global.ownernomer = "242068170944"
 global.ownername = "🌹MR KEVIN TSH🌹"
 global.ytname = "YT: alvinblaste...13"
 global.socialm = "GitHub: Kevin-Tsh"
 global.location = "Cong Kinshasa, Haut Katanga, Lubumbashi"
 
-global.ownernumber = '243891716833'  //creator number
+global.ownernumber = '242068170944'  //creator number
 global.ownername = '🌹𝐌𝐑.𝐊É𝐕𝐈𝐍 𝐓𝐒𝐇🌹' //owner name
 global.botname = '🐲⸢𝐆𝐎𝐊𝐔-𝐌𝐃⸥🐲' //name of the bot
 
